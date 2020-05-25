abbfa494d72943e90ac64fa2b2b7c665781e47bd
Ashnil <ashnilvazirani26@gmail.com>
2020-05-25 16:13:22 +0530

    Add files via upload
    change in schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A valid tour name is must'],
         unique: true,
         trim: true,
+        minlength: [10, 'A tour name must be atleast 10 characters long'],
+        maxlength: [40, 'A tour name must be at most 40 characters long'],
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'Specify the number of days'],
@@ -20,6 +24,10 @@ const tourSchema = new mongoose.Schema({
             true,
             'Let the tourists know about the difficulty level of this tour',
         ],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'A tour must have either of three',
+        },
     },
     price: {
         type: Number,
@@ -28,12 +36,22 @@ const tourSchema = new mongoose.Schema({
     ratingsAverage: {
         type: Number,
         default: 4.5,
+        min: [1, 'At least 1.0 rating'],
+        max: [5, 'Max rate value =5.0'],
     },
     ratingQuantity: {
         type: Number,
         default: 0,
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function (value) {
+                return value < this.price;
+            },
+            message: 'The discounted price should be less than the regular price'
+        }
+    },
     summary: {
         type: String,
         trim: true,
@@ -52,13 +70,61 @@ const tourSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now(),
-        select: false //doesn't send the value
+        select: false, //doesn't send the value
     },
     startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+    toObject: {
+        virtuals: true,
+    },
 });
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
 //Using the regular function instead of arrow function since we need access to 'this' keyword to point to current object
+//this pre function is executed on SAVE event on methods save() and create() and not on insertMany()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, {
+        lower: true
+    });
+    next();
+});
+tourSchema.pre(/^find/, function (next) {
+    this.find({
+        secretTour: {
+            $ne: true
+        }
+    });
+    this.start = Date.now();
+    next();
+});
+tourSchema.post(/^find/, function (doc, next) {
+    console.log(`Query took ${Date.now() - this.start} ms`);
+    next();
+});
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+//AGGREGATE MIDDLEWARE:
+tourSchema.pre('aggregate', function (next) {
+    //excluding the all secret tours to perform aggregate function
+    this.pipeline().unshift({
+        $match: {
+            secretTour: {
+                $ne: true
+            }
+        }
+    });
+    console.log(this.pipeline());
+    next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,64 +1,64 @@
 const fs = require('fs');
 class APIFeature {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-    }
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
 
-    filter() {
-        const queryObj = {
-            ...this.queryString,
-        };
-        //creates a hard copy
-        //1A-NORMAL FILTERARTION
-        const excludedData = ['page', 'sort', 'limit', 'fields'];
-        excludedData.forEach((element) => delete queryObj[element]);
-        console.log(queryObj);
-        //1B-ADVANCED FILERATION: lt,lte, gt, gte
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-        console.log(JSON.parse(queryStr));
+  filter() {
+    const queryObj = {
+      ...this.queryString,
+    };
+    //creates a hard copy
+    //1A-NORMAL FILTERARTION
+    const excludedData = ['page', 'sort', 'limit', 'fields'];
+    excludedData.forEach((element) => delete queryObj[element]);
+    console.log(queryObj);
+    //1B-ADVANCED FILERATION: lt,lte, gt, gte
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-        // const tours = await Tour.find({
-        //   duration: 5
-        // });
+    // const tours = await Tour.find({
+    //   duration: 5
+    // });
 
-        //BUILD THE QUERY:
-        this.query.find(JSON.parse(queryStr));
-        // let query = Tour.find(JSON.parse(queryStr));
-        return this;
+    //BUILD THE QUERY:
+    this.query.find(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr));
+    return this;
+  }
+  sort() {
+    if (this.queryString.sort) {
+      //sort=pirce->ascending order && sort=-price-> descending order
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
     }
-    sort() {
-        if (this.queryString.sort) {
-            //sort=pirce->ascending order && sort=-price-> descending order
-            const sortBy = this.queryString.sort.split(',').join(' ');
-            console.log(sortBy);
-            this.query = this.query.sort(sortBy);
-        } else {
-            this.query = this.query.sort('-createdAt');
-        }
-        return this;
-    }
+    return this;
+  }
 
-    limitFields() {
-        if (this.queryString.fields) {
-            const fields = this.queryString.fields.split(',').join(' ');
-            this.query = this.query.select(fields);
-        } else {
-            this.query = this.query.select('-__v');
-        }
-        return this;
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
     }
-    paginate() {
-        const page = (this.queryString.page) ? this.queryString.page * 1 : 1;
-        const limit = (this.queryString.limit) ? this.queryString.limit * 1 : 2;
-        const skip = (page - 1) * limit;
-        if (page == 1) {
-            this.query = this.query.limit(limit);
-        } else if (page >= 2) {
-            this.query = this.query.skip(skip).limit(limit);
-        }
-        return this;
+    return this;
+  }
+  paginate() {
+    const page = this.queryString.page ? this.queryString.page * 1 : 1;
+    const limit = this.queryString.limit ? this.queryString.limit * 1 : 20;
+    const skip = (page - 1) * limit;
+    if (page == 1) {
+      this.query = this.query.limit(limit);
+    } else if (page >= 2) {
+      this.query = this.query.skip(skip).limit(limit);
     }
+    return this;
+  }
 }
 module.exports = APIFeature;
