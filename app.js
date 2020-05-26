f3a4871647b05d56875da77526b17337bce16f66
Ashnil <ashnilvazirani26@gmail.com>
2020-05-26 21:25:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/appError');
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
@@ -17,6 +17,7 @@ app.use(express.static(`${__dirname}/public`));
 // });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+ś;
 //Developer created middleware
 app.use((request, response, next) => {
   request.myTime = new Date().toISOString();
@@ -32,5 +33,26 @@ app.get(`/`, (request, response) => {
 app.post('/', (request, response) => {
   console.log(request.body);
 });
-
+app.all('*', (request, response, next) => {
+  // response.status(404).json({
+  //   status: 'Failer to get the url',
+  //   message: `Cant fetch the ${request.originalUrl} on this server`
+  // })
+  // next();
+  // const err = new Error(`Cant fetch the ${request.originalUrl} on this server`);
+  // err.status = 'fail-to-requeted-url';
+  // err.statusCode = 404;
+  // next(err);
+  next(
+    new AppError(`Cant fetch the ${request.originalUrl} on this server`, 404)
+  );
+});
+app.use((error, request, response, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+  response.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+  });
+});
 module.exports = app;
