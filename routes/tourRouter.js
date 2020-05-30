4c72bd21e59db4b2176dfc843c74e4b7ab85a8b3
Ashnil <ashnilvazirani26@gmail.com>
2020-05-30 19:42:25 +0530

    Add files via upload

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -16,6 +16,6 @@ tourRouter
     .route('/:id')
     .get(tourController.getTour)
     .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+    .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 module.exports = tourRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,10 @@ const authController = require('./../controllers/authController')
 
 userRouter.post('/signup', authController.signup);
 userRouter.post('/login', authController.login);
+userRouter.post('/forgotpassword', authController.forgotPassword);
+userRouter.patch('/resetPassword/:token', authController.resetPassword);
+userRouter.patch('/updatePassword', authController.protect, authController.updatePassword);
+
 userRouter
     .route('/')
     .get(userController.getAllUsers)
